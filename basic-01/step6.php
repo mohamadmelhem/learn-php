5316f92c0987bee51f3e2ace43ff3314fcb30c2b
mohamadmelhem <mhamadmlhm0@gmail.com>
2023-02-17 16:48:19 +0200

    step 6

---
+++ basic-01/step6.php
@@ -0,0 +1,11 @@
+
+<?php
+
+// Define a function that takes two parameters and returns their sum
+function calculateArea($height, $width) {
+  $area = $height * $width;
+  echo"area is $area";
+}
+calculateArea(5, 3);
+?>
+
