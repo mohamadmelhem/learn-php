7e5f181c0b52e707c9d3db41ea1fef31d582627e
mohamadmelhem <mhamadmlhm0@gmail.com>
2023-02-17 16:10:52 +0200

    step 3

---
+++ basic-01/step3.php
@@ -0,0 +1,9 @@
+<?php 
+$name = 'mohamad';
+$email = 'mhamadmlhm0@gmail.com';
+echo "my name is $name my email is $email " ;
+$height = '10';
+$width = '15' ;
+$area = $height * $width ;
+print $area ;
+?>
