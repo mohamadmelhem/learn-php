cbc9cc9b383a1f9a4740db2204302b88e06284c1
mohamadmelhem <mhamadmlhm0@gmail.com>
2023-02-19 20:19:19 +0200

    basic-2 step 5

---
+++ basic-02/step-05.php
@@ -0,0 +1,30 @@
+<?php
+function wordToDigit($word) {
+  switch ($word) {
+    case "zero":
+      return 0;
+    case "one":
+      return 1;
+    case "two":
+      return 2;
+    case "three":
+      return 3;
+    case "four":
+      return 4;
+    case "five":
+      return 5;
+    case "six":
+      return 6;
+    case "seven":
+      return 7;
+    case "eight":
+      return 8;
+    case "nine":
+      return 9;
+    default:
+      return false;
+  }
+}
+
+
+?>
